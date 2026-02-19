02eba886ef02bd2fb07b930d48898f3cbd8f7260
chelc <chelsea.diane.paquibot@gmail.com>
2026-02-19 23:37:28 +0800

    homepage
    click image fade into different car, will make button for it soon

--- homepage.js
+++ homepage.js
@@ -9,3 +9,41 @@ slider.oninput = () => {
   dragLine.style.left = sliderVal + "%"; // Move drag line
   img.style.width = sliderVal + "%"; // Adjust the width of the second image
 }
+
+async function showSecond(target) {
+  let second = document.getElementById('second');
+  await fadeOut(target);
+  await fadeIn(second);
+}
+
+async function showFirst(target) {
+  let first = document.getElementById('first');
+  await fadeOut(target);
+  await fadeIn(first);
+}
+
+function fadeOut(target, duration = 500) {
+  return new Promise(resolve => {
+    const animationEnded = () => {
+        target.style.display = 'none';
+        target.onanimationend = null;
+        target.style.animation =  null;
+        resolve();
+    };
+    target.onanimationend = animationEnded;
+    target.style.animation = `fade-out ${duration}ms 1`;
+  })
+}
+
+function fadeIn(target, duration = 500, display = 'block') {
+  return new Promise(resolve => {
+    const animationEnded = () => {
+        target.onanimationend = null;
+        target.style.animation =  null;
+        resolve();
+    };
+    target.animationend = animationEnded;
+     target.style.display = display;
+    target.style.animation = `fade-in ${duration}ms 1`;
+  })
+}

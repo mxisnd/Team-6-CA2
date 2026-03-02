a5127df4eff53f707046d51ce7675ff11aa46078
chelc <chelsea.diane.paquibot@gmail.com>
2026-03-03 04:54:53 +0800

    homepage
    text animations

--- homepage.js
+++ homepage.js
@@ -1,4 +1,78 @@
+// Image 1 Text Anim
+const paragraph1 = document.querySelectorAll(".image-1-text");
 
+document.addEventListener("scroll", function () {
+  paragraph1.forEach((paragraph) => {
+    if (isInView1(paragraph)) {
+      paragraph.classList.add("image-1-text--visible")
+    }
+  });
+});
+
+function isInView1 (element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.bottom > 0 &&
+    rect.top < (window.innerHeight - 150 || document.documentElement.clientHeight - 150)
+  );
+}
+
+// Image 2 Text Anim
+const paragraph2 = document.querySelectorAll(".image-2-text");
+
+document.addEventListener("scroll", function () {
+  paragraph2.forEach((paragraph) => {
+    if (isInView2(paragraph)) {
+      paragraph.classList.add("image-2-text--visible")
+    } else paragraph.classList.remove("image-2-text--visible")
+  });
+});
+
+function isInView2 (element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.bottom > 0 &&
+    rect.top < (window.innerHeight - 150 || document.documentElement.clientHeight - 150)
+  );
+}
+
+// Image 3 Text Anim
+const paragraph3 = document.querySelectorAll(".image-3-text");
+
+document.addEventListener("scroll", function () {
+  paragraph3.forEach((paragraph) => {
+    if (isInView3(paragraph)) {
+      paragraph.classList.add("image-3-text--visible")
+    } else paragraph.classList.remove("image-3-text--visible")
+  });
+});
+
+function isInView3 (element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.bottom > 0 &&
+    rect.top < (window.innerHeight - 150 || document.documentElement.clientHeight - 150)
+  );
+}
+
+// Image 3 Subtext Text Anim
+const paragraph4 = document.querySelectorAll(".image-3-subtext-1");
+
+document.addEventListener("scroll", function () {
+  paragraph4.forEach((paragraph) => {
+    if (isInView4(paragraph)) {
+      paragraph.classList.add("image-3-subtext-1--visible")
+    } else paragraph.classList.remove("image-3-subtext-1--visible")
+  });
+});
+
+function isInView4 (element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.bottom > 0 &&
+    rect.top < (window.innerHeight - 400 || document.documentElement.clientHeight - 400)
+  );
+}
 
 //button click
 

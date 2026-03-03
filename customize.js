fb1d8ecefc70c4b07566db6c9df0094fc361a895
n4tellaa <nataliewongqien@gmail.com>
2026-03-03 12:57:29 +0800

    customize
    lighting adjustment

--- customize.js
+++ customize.js
@@ -18,7 +18,7 @@ container.appendChild(renderer.domElement);
 const ambientLight = new THREE.AmbientLight(0xffffff, 2);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(5, 5, 5);
 scene.add(directionalLight);
 
